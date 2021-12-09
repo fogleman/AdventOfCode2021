a8e204d20a878c46ebc82981bcf76ce5d32f7df5
Michael Fogleman <fogleman@gmail.com>
2021-12-08 19:08:31 -0500

    day 8

--- 08.py
+++ 08.py
@@ -1,21 +1,19 @@
 import fileinput
 
-from itertools import permutations
+from itertools import *
 
-entries = [(line.split()[:10], line.split()[-4:])
-    for line in fileinput.input()]
+entries = [(x.split()[:10], x.split()[-4:]) for x in fileinput.input()]
 
 print(sum(len(x) in [2, 3, 4, 7] for e in entries for x in e[1]))
 
 def solve(patterns, outputs):
-    numbers = [
+    nums = [
         'abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf',
-        'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg',
-    ]
-    for m in [dict(zip('abcdefg', p)) for p in permutations('abcdefg')]:
-        convert = lambda x: ''.join(sorted(m[c] for c in x))
-        if {convert(x) for x in patterns} != set(numbers):
-            continue
-        return int(''.join(str(numbers.index(convert(x))) for x in outputs))
+        'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg']
+    for p in permutations('abcdefg'):
+        def convert(x):
+            return ''.join(sorted(chr(ord('a') + p.index(c)) for c in x))
+        if all(convert(x) in nums for x in patterns):
+            return int(''.join(str(nums.index(convert(x))) for x in outputs))
 
 print(sum(solve(*e) for e in entries))
