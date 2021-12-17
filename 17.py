6c09faa42429d343c8f33ba3c0fe739ff26ff111
Michael Fogleman <fogleman@gmail.com>
2021-12-17 18:45:40 -0500

    faster

--- 17.py
+++ 17.py
@@ -18,18 +18,17 @@ def run(v, bounds):
         vy -= 1
         if x0 <= x <= x1 and y0 <= y <= y1:
             return hi
-        if y < y0 and vy < 0:
+        if (y < y0 and vy < 0) or (x < x0 and vx <= 0) or (x > x1 and vx >= 0):
             return None
 
 n = hi = count = 0
 while True:
     n += 1
     for vy in range(-n, n + 1):
-        for vx in range(-n, n + 1):
-            if vx not in (-n, n) and vy not in (-n, n):
-                continue
+        for vx in range(-n, n + 1) if vy in [-n, n] else [-n, n]:
             y = run((vx, vy), bounds)
-            if y is not None:
-                count += 1
-                hi = max(hi, y)
-                print(hi, count, (vx, vy))
+            if y is None:
+                continue
+            count += 1
+            hi = max(hi, y)
+            print(hi, count, (vx, vy))
